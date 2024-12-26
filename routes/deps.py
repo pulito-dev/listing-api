478e056c578954322495a112a868e8a868d89000
isakal <sakalivan4@gmail.com>
2024-12-26 13:39:33 +0100

    Try fixing the session overflow issue with db

--- routes/deps.py
+++ routes/deps.py
@@ -4,4 +4,9 @@ from collections.abc import Generator
 
 def get_db() -> Generator[Session, None, None]:
     with Session(db_cl.engine) as session:
+        # return the db session
         yield session
+
+        # close session so queue pool doesn't overflow
+        session.close()
+        
